fba116f4a5048d91b0c6a717c08360453cd1de4a
fatima643 <fatimasehar912@gmail.com>
2024-08-07 19:14:18 +0500

    completed project

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-
-
-
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === "none" || sidebar.style.display === "") {
+        sidebar.style.display = "block";
+        setTimeout(() => {
+            sidebar.style.transform = "translateX(0)";
+        }, 10); // Add a small delay to allow the display change to take effect before the transform
+    } else {
+        sidebar.style.transform = "translateX(-100%)";
+        sidebar.addEventListener('transitionend', () => {
+            sidebar.style.display = "none";
+        }, {
+            once: true
+        });
+    }
+}
